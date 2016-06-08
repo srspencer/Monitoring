e78c564875af8ecef57369d0241c8bf78ec83a38
Spence <srspencer@gmail.com>
2016-06-08 15:42:32 -0400

    new JavaVersion module

---
+++ JavaVersion.java
@@ -0,0 +1,15 @@
+public class JavaVersion{
+	String v;
+
+	public JavaVersion(){
+		v = System.getProperty("java.version");
+	}
+
+	public String getJavaVersion(){
+		return(v);
+	}
+
+	public static void main(String[] args){
+		System.out.println(new JavaVersion().getJavaVersion());
+	}
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main{
 	public static void main(String[] args){
+		JavaVersion v = new JavaVersion();
+		System.out.println(v.getJavaVersion());
 		FreeMem f = new FreeMem();
 		System.out.println(f.getFree());
 		System.out.println(f.getMax());
-		EnvMap e = new EnvMap();
+//		EnvMap e = new EnvMap();
 	}
 }
